edf35bf75739564ce7e2dc270d89995474305dd1
jesse van vuuren <ikpower100@gmail.com>
2021-12-03 15:22:20 +0100

    testerijerkik

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -8,6 +9,11 @@ const envTest = process.env.TEST || "not"
 app.get('/', (req, res) => {
     res.send('Hello World! env is: ' + envTest)
 })
+
+app.get('/movie', (req, res) => {
+    res.sendFile(path.resolve('pages/index.html'))
+    //res.send("id: " + req.query.id)
+})  
 
 app.listen(port, () => {
     console.log("Example app listening at http://localhost:" + port)
