904b8bc62fc58e6e34c63b3a018abd6619c80948
jesse van vuuren <ikpower100@gmail.com>
2021-12-03 15:53:14 +0100

    title test ?id=

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
+const fs = require("fs")
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -10,11 +11,31 @@ app.get('/', (req, res) => {
     res.send('Hello World! env is: ' + envTest)
 })
 
+
 app.get('/movie', (req, res) => {
-    res.sendFile(path.resolve('pages/index.html'))
-    //res.send("id: " + req.query.id)
-})  
+    fs.readFile(path.resolve('pages/index.html'), 'utf8', (err, htmlData) => {
+        if (err) {
+            console.error('Error during file reading', err);
+            return res.status(404).end()
+        }
+        // get post info
+        const postId = req.query.id;
+        if (!postId) return res.redirect('/');
+
+        //inject meta tags
+        htmlData = htmlData
+            .replace("<title>Movie Recommender</title>", "<title>titleText: " + postId + "</title>")
+            // .replace('__META_OG_TITLE__', post.title)
+            // .replace('__META_OG_DESCRIPTION__', post.description)
+            // .replace('__META_DESCRIPTION__', post.description)
+            // .replace('__META_OG_IMAGE__', post.thumbnail)
+
+        return res.send(htmlData);
+    });
+})
+
 
 app.listen(port, () => {
     console.log("Example app listening at http://localhost:" + port)
 })
+
