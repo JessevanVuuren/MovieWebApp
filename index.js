65296a290335c030908fd5e089ea1b25207c9fbb
jesse van vuuren <ikpower100@gmail.com>
2021-12-03 15:16:53 +0100

    env test

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000
+const envTest = process.env.TEST || "not"
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! env is: ' + envTest)
 })
 
 app.listen(port, () => {
-    console.log("Example app listening at http://localhost: " + port)
+    console.log("Example app listening at http://localhost:" + port)
 })
